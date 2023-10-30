ed3a53ee81dc2706e45be13dbda776e3d0263dad
manda <srinivas911032@gmail.com>
2023-10-31 00:03:13 +0530

    made changes to the Main java file

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,12 +1,12 @@
 package com.example;
 
 import java.util.Scanner;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.apache.logging.log4j.LogManager;
+import com.apache.logging.log4j.Logger;
 
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger(Main.Class);
+    private static final Logger logger = LogManager.getLogger(Main.class);
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
