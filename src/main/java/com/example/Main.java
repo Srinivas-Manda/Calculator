bae2194de64e301c6f25ae4c5b1d1aab813535c7
manda <srinivas911032@gmail.com>
2023-10-31 00:12:12 +0530

    newest bruh

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,8 @@
 package com.example;
 
 import java.util.Scanner;
-import com.apache.logging.log4j.LogManager;
-import com.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class Main {
