771f69d2fd8574872c155db7fc56cadc66347458
manda <srinivas911032@gmail.com>
2023-10-31 01:16:08 +0530

    shubhan push

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,46 +1,7 @@
-import com.example.Main;
-import org.junit.Assert.*;
+package com.example;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
-
-
-// public class CalculatorTest{
-//  private Main calculator;
- 
-//  @Before
-//  public void setUp() {calculator = new Main();}
- 
-//  @Test
-//  public void test_add_positive(){
-//     int a = 1;
-//     int b = 2;
-//     int expectedResult = 3;
-//     Assert.assertEquals(expectedResult, calculator.add(a,b));
-//  }
-//  @Test
-//  public void test_add_negative(){
-//     int a = 1;
-//     int b = 2;
-//     int expectedResult = 0;
-//     Assert.assertNotEquals(expectedResult, calculator.add(a,b));
-//  }
- 
-//  @Test
-//  public void test_sub_positive(){
-//     int a = 2;
-//     int b = 2;
-//     int expectedResult = 0;
-//     Assert.assertNotEquals(expectedResult, calculator.sub(a,b));
-//  }
- 
-//  @Test
-//  public void test_sub_negative(){
-//     int a = 2;
-//     int b = 2;
-//     int expectedResult = 0;
-//     Assert.assertNotEquals(expectedResult, calculator.sub(a,b));
-//  }
-// }
 
 public class CalculatorTest {
     Calculator calcObj = new Calculator();
